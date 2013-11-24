7c131de62928680bc14b6b919b9c0e9802ad4686
Artem Danilov <artyom.danilov@odnoklassniki.ru>
2013-11-24 00:57:15 -0800

    add level check in BST problem

--- src/com/tema/algo/problems/CheckBSTProblem.java
+++ src/com/tema/algo/problems/CheckBSTProblem.java
@@ -1,59 +1,57 @@
 package com.tema.algo.problems;
 
-//TODO: check levels
 public class CheckBSTProblem {
 
     public boolean isBST(Node root){
         try {
-            traverse(root);
+            traverse(root, 0);
             return true;
         } catch (NonBSTException e) {
             return false;
         }
     }
 
-    private Result traverse(Node node){
+    private Result traverse(Node node, int level){
+        level++;
         if (node.left != null && node.right != null){
-            Result left = traverse(node.left);
-            Result right = traverse(node.right);
-            if (left.max > node.value || right.min < node.value){
+            Result left = traverse(node.left, level);
+            Result right = traverse(node.right, level);
+            if (left.max > node.value || right.min < node.value || Math.abs(left.level - right.level) > 1){
                 throw new NonBSTException();
             }
-            return new Result(left.min, right.max);
+            return new Result(left.min, right.max, Math.max(left.level, right.level));
         }
 
         if (node.left != null){
-            Result left = traverse(node.left);
-            if (left.max > node.value){
+            Result left = traverse(node.left, level);
+            if (left.max > node.value || Math.abs(left.level - level) > 1){
                 throw new NonBSTException();
             }
-            return new Result(left.min, node.value);
+            return new Result(left.min, node.value, left.level);
         }
 
         if (node.right != null){
-            Result right = traverse(node.right);
-            if (right.min < node.value){
+            Result right = traverse(node.right, level);
+            if (right.min < node.value || Math.abs(right.level - level) > 1){
                 throw new NonBSTException();
             }
-            return new Result(node.value, right.max);
+            return new Result(node.value, right.max, right.level);
         }
 
-        return new Result(node.value);
+        return new Result(node.value, node.value, level);
 
     }
 
     static class NonBSTException extends RuntimeException{}
 
     static class Result{
+        int level;
         int min, max;
 
-        Result(int v) {
-            this(v, v);
-        }
-
-        Result(int min, int max) {
+        Result(int min, int max, int level) {
             this.min = min;
             this.max = max;
+            this.level = level;
         }
     }
 
